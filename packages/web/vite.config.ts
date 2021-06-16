d4344774fe1d1211fc681caac90877b84efcd5b0
Łukasz Mariański <lmarianski@protonmail.com>
2021-06-16 21:51:45 +0200

    config

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -3,19 +3,22 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  define: { 'process.env': {} },
-  /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
-  resolve: {
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
-    ]
-  },
-  */
+	plugins: [vue()],
+	define: { 'process.env': {} },
+	// build: {
+	// 	target: 'esnext'
+	// }
+	/* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
+	resolve: {
+		extensions: [
+			'.js',
+			'.json',
+			'.jsx',
+			'.mjs',
+			'.ts',
+			'.tsx',
+			'.vue',
+		]
+	},
+	*/
 })
